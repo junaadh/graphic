e976194f59623fbcab34a88366ff6a45276e8e83
Junaadh <junaadh.02@gmail.com>
2024-02-15 17:21:11 +0800

    change to use GraphikBuilder

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::{
+    cell::RefCell,
     fs::{File, OpenOptions},
     io::Write,
+    rc::Rc,
 };
 
 pub const WIDTH: usize = 800;
@@ -69,6 +71,11 @@ impl GraphikCircle {
             color: 0xffffffff,
             center: false,
         }
+    }
+
+    pub fn radius(&mut self, radius: usize) -> Self {
+        self.radius = radius;
+        *self
     }
 
     pub fn origin(&mut self, x0: i32, y0: i32) -> Self {
@@ -173,7 +180,7 @@ impl GraphikLine {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct GraphikBuffer {
     pub width: usize,
     pub height: usize,
@@ -188,39 +195,65 @@ impl GraphikBuffer {
             buffer: vec![0; width * height],
         }
     }
+}
 
-    pub fn fill(mut self, color: u32) -> Self {
-        for pixel in self.buffer.iter_mut() {
-            *pixel = color;
+pub fn get_center(canvas: usize, object: usize) -> i32 {
+    ((canvas - object) / 2) as i32
+}
+
+pub fn lerpf(a: f32, b: f32, t: f32) -> f32 {
+    a + (b - a) * t
+}
+
+#[derive(Debug)]
+pub struct GraphikBuilder {
+    pub buffer: Rc<RefCell<GraphikBuffer>>,
+}
+
+impl GraphikBuilder {
+    pub fn new(width: usize, height: usize) -> Self {
+        Self {
+            buffer: Rc::new(RefCell::new(GraphikBuffer::new(width, height))),
         }
-        self
     }
 
-    pub fn rect_fill(mut self, rect: &mut GraphikRect) -> Self {
+    pub fn fill(&mut self, color: u32) {
+        self.buffer
+            .borrow_mut()
+            .buffer
+            .iter_mut()
+            .for_each(|pixel| {
+                *pixel = color;
+            });
+    }
+
+    pub fn rect_fill(&mut self, rect: &mut GraphikRect) {
+        let mut buffer = self.buffer.borrow_mut();
         if rect.center {
-            let x0 = get_center(self.width, rect.width);
-            let y0 = get_center(self.height, rect.height);
+            let x0 = get_center(buffer.width, rect.width);
+            let y0 = get_center(buffer.height, rect.height);
             rect.origin(x0, y0);
         }
 
         for dy in 0..rect.height {
             let y = rect.y0 as usize + dy;
-            if y < self.height {
+            if y < buffer.height {
                 for dx in 0..rect.width {
                     let x = rect.x0 as usize + dx;
-                    if x < self.width {
-                        self.buffer[y * self.width + x] = rect.color;
+                    if x < buffer.width {
+                        let bufwid = buffer.width;
+                        buffer.buffer[y * bufwid + x] = rect.color;
                     }
                 }
             }
         }
-        self
     }
 
-    pub fn circle_fill(mut self, circle: &mut GraphikCircle) -> Self {
+    pub fn circle_fill(&mut self, circle: &mut GraphikCircle) {
+        let mut buffer = self.buffer.borrow_mut();
         if circle.center {
-            let x0 = (self.width / 2) as i32;
-            let y0 = (self.height / 2) as i32;
+            let x0 = (buffer.width / 2) as i32;
+            let y0 = (buffer.height / 2) as i32;
             circle.origin(x0, y0);
         }
 
@@ -229,24 +262,24 @@ impl GraphikBuffer {
         let x2 = circle.x0 + circle.radius as i32;
         let y2 = circle.y0 + circle.radius as i32;
         for y in y1..y2 {
-            if y < self.height as i32 {
+            if 0 <= y && y < buffer.height as i32 {
                 for x in x1..x2 {
-                    if x < self.width as i32 {
+                    if 0 <= x && x < buffer.width as i32 {
                         let dx = x - circle.x0;
                         let dy = y - circle.y0;
                         if (dx * dx + dy * dy) <= (circle.radius * circle.radius) as i32 {
-                            self.buffer[y as usize * self.width + x as usize] = circle.color;
+                            let bufwid = buffer.width;
+                            buffer.buffer[y as usize * bufwid + x as usize] = circle.color;
                         }
                     }
                 }
             }
         }
-
-        self
     }
 
-    pub fn line_draw(mut self, line: &mut GraphikLine) -> Self {
-        self.process_line_vertices(line);
+    pub fn line_draw(&mut self, line: &mut GraphikLine) {
+        let mut buffer = self.buffer.borrow_mut();
+        self.process_line_vertices(line, buffer.width, buffer.height);
 
         let mut x0 = line.x0;
         let mut y0 = line.y0;
@@ -259,8 +292,9 @@ impl GraphikBuffer {
         let mut err = dx - dy;
 
         while x0 != x1 || y0 != y1 {
-            if 0 <= x0 && x0 < self.width as i32 && 0 <= y0 && y0 < self.height as i32 {
-                self.buffer[y0 as usize * self.width + x0 as usize] = line.color;
+            if 0 <= x0 && x0 < buffer.width as i32 && 0 <= y0 && y0 < buffer.height as i32 {
+                let bufwid = buffer.width;
+                buffer.buffer[y0 as usize * bufwid + x0 as usize] = line.color;
             }
             let e2 = 2 * err;
             if e2 > -dy {
@@ -272,11 +306,10 @@ impl GraphikBuffer {
                 y0 += sy;
             }
         }
-
-        self
     }
 
-    pub fn save_as_ppm(self, file_path: &str) -> Result<(), Error> {
+    pub fn save_as_ppm(&self, file_path: &str) -> Result<(), Error> {
+        let buffer = self.buffer.borrow();
         let mut file = OpenOptions::new()
             .read(true)
             .write(true)
@@ -287,9 +320,9 @@ impl GraphikBuffer {
                 eprintln!("Failed to open file {}: {}", &file_path, err);
                 Error::FileOpenError
             })?;
-        self.write_header(&mut file)?;
+        self.write_header(&mut file, buffer.width, buffer.height)?;
 
-        for pixel in self.buffer.iter() {
+        for pixel in buffer.buffer.iter() {
             let bytes = [
                 (*pixel & 0xff) as u8,
                 ((*pixel >> 8) & 0xff) as u8,
@@ -300,20 +333,20 @@ impl GraphikBuffer {
         Ok(())
     }
 
-    fn write_header(&self, file: &mut File) -> Result<(), Error> {
-        let header = format!("P6\n{} {} 255\n", self.width, self.height);
+    fn write_header(&self, file: &mut File, width: usize, height: usize) -> Result<(), Error> {
+        let header = format!("P6\n{} {} 255\n", width, height);
         file.write_all(header.as_bytes())
             .map_err(|_| Error::FileWriteError)?;
         Ok(())
     }
 
-    fn process_line_vertices(&self, line: &mut GraphikLine) {
+    fn process_line_vertices(&self, line: &mut GraphikLine, width: usize, height: usize) {
         if line.center {
             if line.vertical {
-                let center_x = (self.width / 2) as i32;
+                let center_x = (width / 2) as i32;
                 line.vertical(center_x, line.y0, line.y1);
             } else if line.horizontal {
-                let center_y = (self.height / 2) as i32;
+                let center_y = (height / 2) as i32;
                 line.horizontal(center_y, line.x0, line.x1);
             }
         }
@@ -321,8 +354,4 @@ impl GraphikBuffer {
         // let y1 = self.height as i32 - line.y1;
         // line.end(x1, y1);
     }
-}
-
-pub fn get_center(canvas: usize, object: usize) -> i32 {
-    ((canvas - object) / 2) as i32
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,75 @@
-use graphik::{Error, GraphikBuffer, GraphikCircle, GraphikLine, GraphikRect, HEIGHT, WIDTH};
+use graphik::{
+    lerpf, Error, GraphikBuilder, GraphikCircle, GraphikLine, GraphikRect, HEIGHT, WIDTH,
+};
 
 fn main() -> Result<(), Error> {
+    let background = 0xff202020_u32;
+    let mut builder = GraphikBuilder::new(WIDTH, HEIGHT);
+
     let mut rect = GraphikRect::new(300, 200).center(true).color(0xff2020ff);
-    GraphikBuffer::new(WIDTH, HEIGHT)
-        .fill(0xff20ff20)
-        .rect_fill(&mut rect)
-        .save_as_ppm("target/rectangle.ppm")?;
+    builder.fill(background);
+    builder.rect_fill(&mut rect);
+    builder.save_as_ppm("target/rectangle.ppm")?;
 
     let mut circ = GraphikCircle::new(150).center(true).color(0xff0000ff);
-    GraphikBuffer::new(WIDTH, HEIGHT)
-        .fill(0xffffffff)
-        .circle_fill(&mut circ)
-        .save_as_ppm("target/circle.ppm")?;
+    builder.fill(background);
+    builder.circle_fill(&mut circ);
+    builder.save_as_ppm("target/circle.ppm")?;
 
     let mut line = GraphikLine::new().color(0xff0000ff);
-    GraphikBuffer::new(WIDTH, HEIGHT)
-        .fill(0xff202020)
-        .line_draw2(&mut line)
-        .line_draw2(&mut line.start(WIDTH as i32, 0).end(0, HEIGHT as i32))
-        .line_draw2(&mut line.horizontal_center(0, WIDTH as i32))
-        .line_draw2(&mut line.vertical_center(0, HEIGHT as i32))
-        .line_draw2(&mut line.vertical(WIDTH as i32 / 3, 0, HEIGHT as i32))
-        .line_draw2(&mut line.horizontal(HEIGHT as i32 / 3, 0, WIDTH as i32))
-        .line_draw2(&mut line.start(0, 0).end(WIDTH as i32 / 5, HEIGHT as i32))
-        .line_draw2(&mut line.start(0, 0).end(WIDTH as i32 / 5 * 3, HEIGHT as i32))
-        .save_as_ppm("target/line2.ppm")?;
+    builder.fill(background);
+    builder.line_draw(&mut line);
+    builder.line_draw(&mut line.start(WIDTH as i32, 0).end(0, HEIGHT as i32));
+    builder.line_draw(&mut line.horizontal_center(0, WIDTH as i32));
+    builder.line_draw(&mut line.vertical_center(0, HEIGHT as i32));
+    builder.line_draw(&mut line.vertical(WIDTH as i32 / 3, 0, HEIGHT as i32));
+    builder.line_draw(&mut line.horizontal(HEIGHT as i32 / 3, 0, WIDTH as i32));
+    builder.line_draw(&mut line.start(0, 0).end(WIDTH as i32 / 5, HEIGHT as i32));
+    builder.line_draw(&mut line.start(0, 0).end(WIDTH as i32 / 5 * 3, HEIGHT as i32));
+    builder.save_as_ppm("target/line.ppm")?;
+
+    let rows = 6 * 2;
+    let cols = 8 * 2;
+    let cell_width = WIDTH / cols;
+    let cell_height = HEIGHT / rows;
+    let mut checker = GraphikRect::new(cell_width, cell_height)
+        .origin(0, 0)
+        .color(0xffffffff);
+    builder.fill(0x00000000);
+    for y in 0..rows {
+        for x in 0..cols {
+            if (x + y) % 2 == 0 {
+                builder.rect_fill(
+                    &mut checker.origin((x * cell_width) as i32, (y * cell_height) as i32),
+                );
+            }
+        }
+    }
+    builder.save_as_ppm("target/checkers.ppm")?;
+
+    let mut radius = cell_width;
+    if cell_height < radius {
+        radius = cell_height;
+    }
+    let mut circ = GraphikCircle::new(radius).color(0xff0000ff);
+    builder.fill(background);
+    for y in 0..rows {
+        for x in 0..cols {
+            let u = x as f32 / cols as f32;
+            let v = y as f32 / rows as f32;
+            let t = (u + v) / 2f32;
+
+            builder.circle_fill(
+                &mut circ
+                    .radius(lerpf(radius as f32 / 8.0, radius as f32 / 2.0, t) as usize)
+                    .origin(
+                        (x * cell_width + radius / 2) as i32,
+                        (y * cell_height + radius / 2) as i32,
+                    ),
+            );
+        }
+    }
+    builder.save_as_ppm("target/circle_checkers.ppm")?;
 
     Ok(())
 }
